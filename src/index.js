f19ac4cc7abffcbe65a40a9362bcd8a69ef8eaf1
vsashyn <sashyn.v@gmail.com>
2017-12-18 12:26:11 +0200

    Show file path in console

--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
 export default function({types: t }) {
     return {
         visitor: {
-            StringLiteral(path, options) {
-                // console.log('found a StringLiteral')
-                var description = []
-                for (let expression of path.node.arguments) {
-                    if (description.length === 0) {
-                        let relativePath
-                        let filePath = this.file.log.filename
-                        if (filePath.charAt(0) !== '/') {
-                            relativePath = filePath
-                        } else {
-                            let cwd = process.cwd()
-                            relativePath = filePath.substring(cwd.length + 1)
-                        }
+            StringLiteral(node, state) {
+                console.log(`Found a JSXText: ${state.file.opts.filename}, ${node.start}, ${node.end}, ${node.value}`);
 
-                        let line = expression.loc.start.line
-                        let column = expression.loc.start.column
-                        description.push(`${relativePath}:${line}:${column}:${this.file.code.substring(expression.start, expression.end)}`)
-                    } else {
-                        description.push(this.file.code.substring(expression.start, expression.end))
-                    }
-
-
-                }
-                console.log(description.join(','))
             },
-            JSXText(path, options) {
-                console.log('found a jSXText');
+            JSXText(node, state) {
+                console.log(`Found a JSXText: ${state.file.opts.filename}, ${node.start}, ${node.end}, ${node.value}`);
             }
         }
     }
